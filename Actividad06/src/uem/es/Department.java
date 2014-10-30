4515f0421b38a407f22580f4e163ed07b0e28e26
Guillermo Valenzuela <Guillermo Valenzuela@10.34.82.172>
2014-10-30 17:50:31 +0100

    commit pendiente documentacion y pruebas

--- Actividad06/src/uem/es/Department.java
+++ Actividad06/src/uem/es/Department.java
@@ -1,5 +1,44 @@
 package uem.es;
 
-public class Department {
+public class Department implements java.io.Serializable {
+	
+private static long serialversionUID=1L;
+String name;
+int id;
+int head;
+public Department(){
+	
+}
+public Department(String name, int id, int head){
+	
+}
+public String getName(){
+	return name;
+}
+public int getId(){
+	return id;
+}
+public int getHead(){
+	return head;
+}
+public void setName(String name){
+	 this.name=name;
+}
+public void setId(int id){
+	 this.id=id;
+}
+public void setHead(int head){
+	 this.head=head;
+}
 
+@Override
+public String toString(){
+	return this.name + "\t"+ this.id + "\t" + this.head;
+}
+@Override
+public boolean equals(Object otherDepartament){
+	Department dep = (Department) otherDepartament;
+	if((this.id== dep.id) && (this.head== dep.head) && (this.name== dep.name));
+	return true;
+}
 }
