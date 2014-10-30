4c31f30f248102f25a096b91ec9914782531a134
migue <migue@Miguel-PC>
2014-10-30 18:17:58 +0100

    Crear fichero y carpeta

--- Actividad06/src/uem/es/Department.java
+++ Actividad06/src/uem/es/Department.java
@@ -1,5 +1,25 @@
 package uem.es;
 
-public class Department {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Department {
+	public void CrearFichero(String ruta) {
+		File fichero = new File(ruta);
+		try {
+			//Escritura
+			java.io.BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("archivos/Departamentos.txt"));
+			bufferedWriter.append("Esto es la linea 1");
+			bufferedWriter.flush();
+			bufferedWriter.newLine();
+			bufferedWriter.append("Esto es la linea 2");
+			bufferedWriter.flush();
+ 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
---
+++ Actividad06/src/uem/es/main.java
@@ -0,0 +1,15 @@
+package uem.es;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ruta = "archivos/Departamentos.txt";
+		Department prueba = new Department();
+		prueba.CrearFichero(ruta);
+	}
+
+}
