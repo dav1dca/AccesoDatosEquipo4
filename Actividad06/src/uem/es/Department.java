c5e1b193d190d532bebaaff7d3e22de198f98df8
migue <migue@Miguel-PC>
2014-11-06 18:06:34 +0100

    a medias

--- Actividad06/src/uem/es/Department.java
+++ Actividad06/src/uem/es/Department.java
@@ -4,18 +4,31 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Department {
 	public void CrearFichero(String ruta) {
+		Scanner scan = new Scanner(System.in);
+		String usuario;
+		int usuarios;
 		File fichero = new File(ruta);
+		usuario = scan.nextLine();
+		
 		try {
+			System.out.println("Introduce al usuario, para finalizar introduce fin");
+			System.out.println("id, lastname (20) name (20), job, regis_date, salary, commission, depNumber");
+			java.io.BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("../Actividad06/src/fichero.txt"));
+			
 			//Escritura
-			java.io.BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("archivos/Departamentos.txt"));
-			bufferedWriter.append("Esto es la linea 1");
+			
+			bufferedWriter.append("id, lastname (20) name (20), job, regis_date, salary, commission, depNumber");
 			bufferedWriter.flush();
+			while(usuario==usuario){
 			bufferedWriter.newLine();
-			bufferedWriter.append("Esto es la linea 2");
+			bufferedWriter.append(usuario);
 			bufferedWriter.flush();
+			
+			}
  
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
--- Actividad06/src/uem/es/main.java
+++ Actividad06/src/uem/es/main.java
@@ -7,7 +7,7 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String ruta = "archivos/Departamentos.txt";
+		String ruta = "../Actividad06/src/fichero.txt";
 		Department prueba = new Department();
 		prueba.CrearFichero(ruta);
 	}
